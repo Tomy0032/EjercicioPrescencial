71a34a5ef26a88d07b429602aff80bef5bcb63f4
Tomy0032 <tomas.com9@gmail.com>
2022-08-30 10:09:32 -0300

    Clase Carrera

--- src/Clases/Carrera.java
+++ src/Clases/Carrera.java
@@ -1,5 +1,44 @@
 package Clases;
 
 public class Carrera {
+	
+	private int idCarrera;
+	private String nombre;
+	private Universidad universidad;
+	
+	public Carrera(int idCarrera, String nombre, Universidad universidad) {
+		this.idCarrera = idCarrera;
+		this.nombre = nombre;
+		this.universidad = universidad;
+	}
 
+	public int getIdCarrera() {
+		return idCarrera;
+	}
+
+	public void setIdCarrera(int idCarrera) {
+		this.idCarrera = idCarrera;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Universidad getUniversidad() {
+		return universidad;
+	}
+
+	public void setUniversidad(Universidad universidad) {
+		this.universidad = universidad;
+	}
+
+	@Override
+	public String toString() {
+		return "Carrera [idCarrera=" + idCarrera + ", nombre=" + nombre + ", universidad=" + universidad + "]";
+	}
+	
 }
