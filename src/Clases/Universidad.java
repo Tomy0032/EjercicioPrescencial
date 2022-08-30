36eacaba0aee326a024090e9fb3f79b320539c78
Tomy0032 <tomas.com9@gmail.com>
2022-08-30 10:07:24 -0300

    Clase Universidad

--- src/Clases/Universidad.java
+++ src/Clases/Universidad.java
@@ -1,5 +1,34 @@
 package Clases;
 
 public class Universidad {
+	
+	private int idUniversidad;
+	private String nombre;
+	
+	public Universidad(int idUniversidad, String nombre) {
+		this.idUniversidad = idUniversidad;
+		this.nombre = nombre;
+	}
 
+	public int getIdUniversidad() {
+		return idUniversidad;
+	}
+
+	public void setIdUniversidad(int idUniversidad) {
+		this.idUniversidad = idUniversidad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Universidad [idUniversidad=" + idUniversidad + ", nombre=" + nombre + "]";
+	}
+	
 }
